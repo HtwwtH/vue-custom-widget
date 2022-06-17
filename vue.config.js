4323958e8ef93c14589350733a833c8a53c99043
HtwwtH <alina.l22@mail.ru>
2022-06-17 16:12:02 +0700

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-custom-widget/' : '/',
   transpileDependencies: true,
   chainWebpack: config => {
     config.module
